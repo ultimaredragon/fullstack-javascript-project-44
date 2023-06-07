31db97489f37fc3f8c0694072925959bf95a9f52
Georgiy Plotnikov <ultimaredragon@gmail.com>
2023-06-07 17:45:23 +0300

    added brain-even

---
+++ bin/brain-even.js
@@ -0,0 +1,36 @@
+import readlineSync from 'readline-sync';
+
+function isDivisibleByTwo() {
+  let correctAnswers = 0;
+  let wrongAnswers = 0;
+
+  while (correctAnswers < 3 && wrongAnswers < 3) {
+    // Генерация случайного числа от 1 до 100
+    const randomNumber = Math.floor(Math.random() * 100) + 1;
+
+    // Запросить у пользователя ввод "Yes" или "No"
+    const answer = readlineSync.question(`Число ${randomNumber} делится на два? (Введите "Yes" или "No"): `);
+
+    // Проверить ответ пользователя
+    if (answer.toLowerCase() === 'yes' && randomNumber % 2 === 0) {
+      correctAnswers++;
+      console.log('Правильный ответ!');
+    } else if (answer.toLowerCase() === 'no' && randomNumber % 2 !== 0) {
+      correctAnswers++;
+      console.log('Правильный ответ!');
+    } else {
+      wrongAnswers++;
+      console.log('Неправильный ответ!');
+    }
+  }
+
+  if (correctAnswers === 3) {
+    console.log('Поздравляю! Вы дали три правильных ответа! Двигаемся дальше!');
+    return true;
+  } else {
+    console.log('Вы дали три неправильных ответа. Игра окончена!');
+    return false;
+  }
+}
+
+export default isDivisibleByTwo();
--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,4 @@
 #!/usr/bin/env node
-import name from '../src/cli.js';
+import greeting from '../src/cli.js';
+
+greeting;
