23e72f572bb2c1608db90d749f9e312e8ba5c1c9
Georgiy Plotnikov <ultimaredragon@gmail.com>
2023-06-17 15:24:01 +0300

    bug fixes

--- games/brain-even.js
+++ games/brain-even.js
@@ -5,32 +5,32 @@ import greeting from '../src/cli.js';
 function isDivisibleByTwo() {
   let correctAnswers = 0;
   let wrongAnswers = 0;
-
+  console.log('Answer "yes" if the number is even, otherwise answer "no"')
   while (correctAnswers < 3 && wrongAnswers < 3) {
     // Генерация случайного числа от 1 до 100
     const randomNumber = Math.floor(Math.random() * 100) + 1;
 
     // Запросить у пользователя ввод "Yes" или "No"
-    const answer = readlineSync.question(`Число ${randomNumber} делится на два? (Введите "Yes" или "No"): `);
+    const answer = readlineSync.question(`Question: ${randomNumber} `);
 
     // Проверить ответ пользователя
     if (answer.toLowerCase() === 'yes' && randomNumber % 2 === 0) {
       correctAnswers += 1;
-      console.log('Правильный ответ!');
+      console.log('Correct!');
     } else if (answer.toLowerCase() === 'no' && randomNumber % 2 !== 0) {
       correctAnswers += 1;
-      console.log('Правильный ответ!');
+      console.log('Correct!');
     } else {
       wrongAnswers += 1;
-      console.log('Неправильный ответ!');
+      console.log('"yes" is wrong answer ;(. Correct answer was "No".');
     }
   }
 
   if (correctAnswers === 3) {
-    console.log('Поздравляю! Вы дали три правильных ответа! Двигаемся дальше!');
+    console.log('Congratulation!');
     return true;
   } else {
-    console.log('Вы дали три неправильных ответа. Игра окончена!');
+    console.log('Let\'s try again!');
     return false;
   }
 }
