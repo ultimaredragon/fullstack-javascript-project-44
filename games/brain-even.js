74e5f02f69ba954608f0c508f84806f82b06e640
Georgiy Plotnikov <ultimaredragon@gmail.com>
2023-06-19 19:14:11 +0300

    bugfixes

--- games/brain-even.js
+++ games/brain-even.js
@@ -6,10 +6,10 @@ const user = greeting();
 
 function isDivisibleByTwo() {
   let correctAnswers = 0;
-  let wrongAnswers = 0;
+
   console.log('Answer "yes" if the number is even, otherwise answer "no"');
 
-  while (correctAnswers < 3 && wrongAnswers < 3) {
+  while (correctAnswers < 3) {
     // Генерация случайного числа от 1 до 100
     const randomNumber = Math.floor(Math.random() * 100) + 1;
 
@@ -24,23 +24,18 @@ function isDivisibleByTwo() {
       correctAnswers += 1;
       console.log('Correct!');
     } else if (answer.toLowerCase() === 'yes' && randomNumber % 2 !== 0) {
-      wrongAnswers += 1;
-      console.log('"yes" is wrong answer ;(. Correct answer was "No".')
+      console.log('"yes" is wrong answer ;(. Correct answer was "No".');
+      console.log(`Let's try again, ${user}!`);
+      return false;
     } else if (answer.toLowerCase() === 'no' && randomNumber % 2 === 0) {
-      wrongAnswers += 1;
-      console.log('"no" is wrong answer ;(. Correct answer was "Yes".')
-    } else {
-      console.log('Incorrect input. Please enter "yes" or "no".');
-    }
+      console.log('"no" is wrong answer ;(. Correct answer was "Yes".');
+      console.log(`Let's try again, ${user}!`);
+      return false;
+    };
   };
-
-  if (correctAnswers === 3) {
-    console.log(`Congratulations, ${user}!`);
-    return true;
-  } else {
-    console.log(`Let's try again, ${user}!`);
-    return false;
-  };
+  
+  console.log(`Congratulations, ${user}!`);
+  return true;
 }
 
 isDivisibleByTwo();
